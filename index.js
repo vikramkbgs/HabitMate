c427379126cbd6b33413a002e4474efb64824eef
vikramkbgs@gmail.com <vikramkbgs@gmail.com>
2023-01-30 10:55:53 +0530

    first commit: adding layout

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 
 const port = 8001;
+// use express router
+app.use("/", require("./routes"));
+app.set('view engine','ejs');
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
