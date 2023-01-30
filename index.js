c311e592f0237810de87e78da47eed9a8ded3c23
vikramkbgs@gmail.com <vikramkbgs@gmail.com>
2023-01-30 18:00:30 +0530

    second commit: adding style to habits page

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
+const expressLayouts = require('express-ejs-layouts');
+
+
+app.use(express.static('./assests'));
 
 const port = 8001;
 // use express router
 app.use("/", require("./routes"));
+
+// setup view engine 
+
 app.set('view engine','ejs');
+app.set('views', './views');
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
-
+const homeController = require('../controllers/home_controller');
 const router = express.Router();
 
 console.log(`router is working...`);
+router.get('/',homeController.home);
+router.get("/userHabits", homeController.userHabits);
 
 module.exports = router;
