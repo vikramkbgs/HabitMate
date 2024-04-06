32cb54e6c1c2e598a1b7438140988a207887cff3
vikramkbgs <vikramkbgs@gmail.com>
2024-04-06 16:06:00 +0530

    nosql db fixed

--- controllers/habits_controller.js
+++ controllers/habits_controller.js
@@ -1,33 +1,34 @@
-
 const Habit = require('../models/habit');
 
 
 // This function finds each and every habit in the list and renders it.
-module.exports.load = function(req, res) {
-    Habit.find({}, function(err, habits) {
-        if (err) {
-            console.error("Cannot Fetch data from Database:", err);
-            return res.status(500).send("Error fetching data from database");
-        } else {
-            return res.render('layout', { habit_list: habits });
-        }
-    });
+module.exports.load = async function(req, res) {
+    try {
+        const habits = await Habit.find({}).exec();
+        return res.render('layouts', { habit_list: habits });
+    } catch (err) {
+        console.error("Error fetching data from Database:", err);
+        return res.status(500).send("Error fetching data from database");
+    }
 }
 
+// This function helps in adding a habit in the list.
+module.exports.add = function (req, res) {
+    // Set default values for habit
+    const habitData = {
+        user: req.body.user || "AnyUser",
+        habit_name: req.body.habit_name || "Unnamed Habit",
+        record_tracker: req.body.record_tracker || {}
+    };
 
-// This function helps in adding a habit in list.
-module.exports.add = function (request, response) {
-    request.body.record_tracker = {};
-    request.body.user = "AnyUser";
-    console.log(request.body);
-    Habit.create(request.body, function (err, newhabit) {
+    // Create new habit
+    Habit.create(habitData, function (err, newHabit) {
         if (err) {
-            console.error("Cannot Create Data");
-            return;
+            console.error("Error creating habit:", err);
+            return res.status(500).send("Error creating habit");
+        } else {
+            console.log("New habit created:", newHabit);
+            return res.redirect("back");
         }
-        else {
-            console.log(newhabit);
-            return response.redirect("back");
-        }
-    })
+    });
 }
